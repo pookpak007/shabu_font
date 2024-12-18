c68da0e03bde4db59d83818ded5e5d2ead6d0d8c
pookpak007 <polarit007@hotmail.com>
2024-12-18 17:28:06 +0700

    เพิ่มหน้า home

---
+++ src/app/pages/pagehome/pagehome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagehome',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagehome.component.html',
+  styleUrl: './pagehome.component.css'
+})
+export class PagehomeComponent {
+
+}
