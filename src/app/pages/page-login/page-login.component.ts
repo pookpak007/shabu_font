0fef6b3dd7cbabfd511eb72fa55882b11d8b2892
pookpak007 <polarit007@hotmail.com>
2024-12-20 17:29:36 +0700

    อัพเดทหมด

--- src/app/pages/page-login/page-login.component.ts
+++ src/app/pages/page-login/page-login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit} from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-page-login',
   standalone: true,
-  imports: [],
+  imports: [
+    InputTextModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './page-login.component.html',
   styleUrl: './page-login.component.css'
 })
 export class PageLoginComponent {
 
+    formGroup!: FormGroup;
+
+
+
+    ngOnInit() {
+        this.formGroup = new FormGroup({
+            text: new FormControl<string | null>(null)
+        });
+    }
 }
